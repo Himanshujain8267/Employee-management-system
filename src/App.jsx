974a5deeae82bbd59b64322b88a26df049e5f012
Himanshujain8267 <Himanshujain8267@gmail.com>
2026-05-26 06:05:47 +0530

    update logout button

--- src/App.jsx
+++ src/App.jsx
@@ -14,28 +14,45 @@ const authData = useContext(AuthContext) ;
 
 
 
-useEffect(()=>{
- 
-    const loggedInUser = localStorage.getItem("loggedInUser") ;
-if(loggedInUser){
- console.log(loggedInUser) ;
-} 
-},[]) ;
+useEffect(() => {
 
+ const loggedInUser =
+ localStorage.getItem("loggedInUser")
+
+ if(loggedInUser){
+
+   const userData =
+   JSON.parse(loggedInUser)
+
+   setLoggedInUser(userData)
+
+   if(userData.email == 'admin@me.com'){
+      setUser('admin')
+   }
+   else{
+      setUser('employee')
+   }
+
+ }
+
+},[])
 
 const handleLogin = (email,password) => {
 
 if(email == 'admin@me.com' && password == '123') {
   setUser('admin') ;
   setLoggedInUser(authData.admin[0]) ;
-  localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'})) ;
+  localStorage.setItem('loggedInUser',JSON.stringify(authData.admin[0])) ; 
 }
 else if(authData ){
   const employee = authData.employees.find((e)=>email == e.email && password == e.password)
   if(employee){
     setUser('employee') ;
     setLoggedInUser(employee) ;
-    localStorage.setItem('loggedInUser',JSON.stringify({role:'employee'})) ;
+    localStorage.setItem(
+'loggedInUser',
+JSON.stringify(employee)
+)
   }
 }
 else{
--- src/components/Others/Header.jsx
+++ src/components/Others/Header.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 
 const Header = ({ data }) => {
+  const logOutUser = () => {
+    localStorage.removeItem('loggedInUser') ;
+    window.location.reload() ;
+  }
   return (
    <div className="flex item-end justify-between text-white ">
 <h1 className='text-2xl '>Hello, <br/> <span className='font-bold' > {data.firstName}</span></h1>
-<button className='cursor-pointer bg-red-600 text-lg font-medium text-white px-3 py-2 rounded-md h-10'>Log Out</button>
+<button onClick={logOutUser} className='cursor-pointer bg-red-600 text-lg font-medium text-white px-3 py-2 rounded-md h-10'>Log Out</button>
 
 
 
